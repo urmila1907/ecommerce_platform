b206a7a0a52d8514da750847b606befab24197b1
urmila1907 <urmilasangwan744@gmail.com>
2025-01-10 08:59:41 +0530

    Creating order route

---
+++ backend/models/Order.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const orderSchema = new mongoose.Schema({
+    product:{
+        type: Schema.Types.ObjectId,
+        ref: 'Product',
+        required: true
+    },
+    quantity: {type: Number, required: true, min: 1},
+    orderDate: {type: Date, default: Date.now},
+    totalCost: {type: Number, required: true},
+    customer:{
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+});
+
+const Order = orderSchema.model("Order", orderSchema);
+module.exports = Order;
