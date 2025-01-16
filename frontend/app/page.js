24ac86eff0cc826394562027096a512adf3b89e9
urmila1907 <urmilasangwan744@gmail.com>
2025-01-16 17:33:29 +0530

    Navbar in home page

---
+++ frontend/app/page.js
@@ -0,0 +1,20 @@
+import Navbar from "@/components/Navbar";
+
+export default function Home() {
+  return (
+    <div>
+      <main >
+        <Navbar/>
+        <h1 style={styles.tagline}>Welcome to my e-commerce store!</h1>
+      </main>   
+    </div>
+  );
+}
+
+const styles = {
+  tagline: {
+    color: "#456",
+    padding: "1rem",
+    textAlign: "center",
+  }
+}
