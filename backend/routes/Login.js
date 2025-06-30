b07501b75d427caf0992da68740c0f6685df9ad6
urmila1907 <urmilasangwan744@gmail.com>
2025-06-30 15:48:34 +0530

    token

--- backend/routes/Login.js
+++ backend/routes/Login.js
@@ -22,7 +22,7 @@ router.post('/', async (req,res)=>{
             secure: process.env.NODE_ENV === 'production', // HTTPS only in production
             sameSite: 'none',
             maxAge: 3600000, // 1 hour in milliseconds
-            path: "/user",
+            path: "/",
         });
         res.cookie("refreshToken", refreshToken, {
             httpOnly: true,
